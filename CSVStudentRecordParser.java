33705721900fd92be09f0f052fc6104de8d67ce7
Szyrex <sachinarunachalam54@gmail.com>
2026-09-04 20:04:22 +0530

    QUESTION 2

---
+++ CSVStudentRecordParser.java
@@ -0,0 +1,32 @@
+public class CSVStudentRecordParser {
+
+    static void parseStudentRecord(String csvLine) {
+
+        String[] fields = csvLine.split(",");
+
+        if (fields.length != 3) {
+
+            System.out.println("Invalid Record");
+
+        } else {
+
+            String name = fields[0];
+            String rollNumber = fields[1];
+            String department = fields[2];
+
+            System.out.println(
+                "Name: " + name +
+                " | Roll No: " + rollNumber +
+                " | Dept: " + department
+            );
+        }
+    }
+
+    public static void main(String[] args) {
+
+        String csvLine =
+            "Ananya Verma,RA2211003010123,CSE";
+
+        parseStudentRecord(csvLine);
+    }
+}
