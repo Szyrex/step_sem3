0faaf7f923d8f1309ffcf376772f9bf87ecc55ff
Szyrex <sachinarunachalam54@gmail.com>
2026-09-24 15:10:22 +0530

    QUESTION 5

---
+++ Main5.java
@@ -0,0 +1,32 @@
+class Employee {
+    String empName;
+    double salary;
+
+    static String companyName = "Bright Horizon Technologies";
+    static int employeeCount = 0;
+
+    Employee(String empName, double salary) {
+        this.empName = empName;
+        this.salary = salary;
+        employeeCount++;
+    }
+
+    static void printCompanyInfo() {
+        System.out.println(companyName);
+        System.out.println("Employees on record: " + employeeCount);
+    }
+}
+
+public class Main5 {
+    public static void main(String[] args) {
+
+        Employee e1 = new Employee("Arjun", 45000);
+        Employee e2 = new Employee("Priya", 50000);
+        Employee e3 = new Employee("Rahul", 55000);
+
+        System.out.println("3 Employee objects created");
+        System.out.println();
+
+        Employee.printCompanyInfo();
+    }
+}
